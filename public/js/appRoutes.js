b1c8ff539f0255e2556ce3b52330ef96b2371cf5
eoghanmartin <martine4@tcd.ie>
2016-01-24 20:17:21 +0000

    Basic working

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -8,13 +8,13 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			controller: 'MainController'
 		})
 
-		.when('/nerds', {
-			templateUrl: 'views/nerd.html',
+		.when('/oppurtunities', {
+			templateUrl: 'views/oppurtunities.html',
 			controller: 'NerdController'
 		})
 
-		.when('/geeks', {
-			templateUrl: 'views/geek.html',
+		.when('/organisations', {
+			templateUrl: 'views/organisations.html',
 			controller: 'GeekController'	
 		});
 
