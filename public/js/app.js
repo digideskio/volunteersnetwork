3162b2624afbdd495edb4e8f87efca959d1b46fd
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-20 03:59:38 +0000

    cleaned up public/js folder

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+var app = angular.module('sampleApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+	$routeProvider
+		// home page
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'MainController'
+		});
+
+		// .when('/organisations', {
+		// 	templateUrl: 'views/organisations.html',
+		// 	controller: 'GeekController'
+		// });
+
+	$locationProvider.html5Mode(true);
+
+}]);
--- public/js/controllers/GeekCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
-
-	$scope.tagline = 'Content on organisations...';	
-
-});
